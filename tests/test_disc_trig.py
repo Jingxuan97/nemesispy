694118fc9ce404a4383bd895b5d0f52bb97b90c6
Jingxuan Yang <jingxuan.yang@hertford.ox.ac.uk>
2021-10-19 17:56:42 +0100

    restructure test folder location

--- tests/test_disc_trig.py
+++ tests/test_disc_trig.py
@@ -25,15 +25,15 @@ class TestDisc(unittest.TestCase):
         for vector in [[1,0,0],[0,1,0],[0,0,1]]:
             np.testing.assert_array_equal(rotatey(vector,0),vector)
             np.testing.assert_array_equal(np.around(rotatey(vector,2*np.pi),10),vector)
-            
+
         np.testing.assert_array_equal(np.around(rotatey([1,0,0],0.5*np.pi),10),[0,0,-1])
         np.testing.assert_array_equal(np.around(rotatey([1,0,0],1*np.pi),10),[-1,0,0])
         np.testing.assert_array_equal(np.around(rotatey([1,0,0],1.5*np.pi),10),[0,0,1])
-        
+
         np.testing.assert_array_equal(np.around(rotatey([0,1,0],0.5*np.pi),10),[0,1,0])
         np.testing.assert_array_equal(np.around(rotatey([0,1,0],1*np.pi),10),[0,1,0])
         np.testing.assert_array_equal(np.around(rotatey([0,1,0],1.5*np.pi),10),[0,1,0])
-        
+
         np.testing.assert_array_equal(np.around(rotatey([0,0,1],0.5*np.pi),10),[1,0,0])
         np.testing.assert_array_equal(np.around(rotatey([0,0,1],1*np.pi),10),[0,0,-1])
         np.testing.assert_array_equal(np.around(rotatey([0,0,1],1.5*np.pi),10),[-1,0,0])
@@ -43,26 +43,26 @@ class TestDisc(unittest.TestCase):
         for vector in [[1,0,0],[0,1,0],[0,0,1]]:
             np.testing.assert_array_equal(rotatez(vector,0),vector)
             np.testing.assert_array_equal(np.around(rotatez(vector,2*np.pi),10),vector)
-            
+
         np.testing.assert_array_equal(np.around(rotatez([1,0,0],0.5*np.pi),10),[0,1,0])
         np.testing.assert_array_equal(np.around(rotatez([1,0,0],1*np.pi),10),[-1,0,0])
         np.testing.assert_array_equal(np.around(rotatez([1,0,0],1.5*np.pi),10),[0,-1,0])
-        
+
         np.testing.assert_array_equal(np.around(rotatez([0,1,0],0.5*np.pi),10),[-1,0,0])
         np.testing.assert_array_equal(np.around(rotatez([0,1,0],1*np.pi),10),[0,-1,0])
         np.testing.assert_array_equal(np.around(rotatez([0,1,0],1.5*np.pi),10),[1,0,0])
-        
+
         np.testing.assert_array_equal(np.around(rotatez([0,0,1],0.5*np.pi),10),[0,0,1])
         np.testing.assert_array_equal(np.around(rotatez([0,0,1],1*np.pi),10),[0,0,1])
         np.testing.assert_array_equal(np.around(rotatez([0,0,1],1.5*np.pi),10),[0,0,1])
-        
+
     def test_generate_angles(self):
         # more work need to be done here to test properly
         zen, azi, lat, lon = generate_angles(180,0,0)
-        
+
     def test_gauss_lobatto_weights(self):
         # more work need to be done here to test properly
-        
+
         # test invalid quadrature ring number input (invalid nmu)
         with self.assertRaises(Exception) as cm:
             gauss_lobatto_weights(phase=0, nmu=0)
@@ -93,18 +93,18 @@ for phase in np.linspace(0,360):
     dtr = np.pi/180
     z_term = np.linspace(-1,1,201)
     theta_term = 2*np.pi - np.arccos(z_term)
-    if 0<= phase <= 180: 
+    if 0<= phase <= 180:
         theta_term = 2*np.pi - np.arccos(z_term)
     else:
         theta_term = np.arccos(z_term)
     x_term = np.around(np.sin(theta_term),14) * np.around(np.cos(phase*dtr),14)
-    
-    alpha =  np.arccos(z_term)+np.pi/2. 
+
+    alpha =  np.arccos(z_term)+np.pi/2.
     minus_r_sin_theta = np.around(np.cos(alpha),14)
     old_x_term = minus_r_sin_theta * np.around(np.cos((phase)*np.pi/180.), 14)
     if (phase > 180.0):
         old_x_term = -old_x_term
-    
+
     # print(old_x_term-x_term)
     print(sum(old_x_term-x_term))
 """
