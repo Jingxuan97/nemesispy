68de877012873c425d5999ecfed35d2f9463c0aa
Jingxuan Yang <jingxuan.yang@hertford.ox.ac.uk>
2021-12-20 00:41:51 +0000

    CIA work in progress

--- nemesispy/radtran/k5cia_trial.py
+++ nemesispy/radtran/k5cia_trial.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sys
+from numpy.core.numeric import isfortran
 from scipy.io import FortranFile
+
+from nemesispy2022.nemesispy.radtran.k0run import T_layer
 
 """
 @param INORMAL: int,
@@ -15,6 +18,38 @@ from scipy.io import FortranFile
 @param NWAVE: int,
     Number of spectral points over which the CIA data is defined
 """
+
+def find_nearest(array, value):
+
+    """
+    FUNCTION NAME : find_nearest()
+
+    DESCRIPTION : Find the closest value in an array
+
+    INPUTS :
+
+        array :: List of numbers
+        value :: Value to search for
+
+    OPTIONAL INPUTS: none
+
+    OUTPUTS :
+
+        closest_value :: Closest number to value in array
+        index :: Index of closest_value within array
+
+    CALLING SEQUENCE:
+
+        closest_value,index = find_nearest(array,value)
+
+    MODIFICATION HISTORY : Juan Alday (29/04/2019)
+
+    """
+
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx],idx
+
 def read_cia(filepath,dnu=10,npara=0):
 
     if npara != 0:
@@ -45,7 +80,109 @@ def read_cia(filepath,dnu=10,npara=0):
     return NU_GRID, TEMPS, K_CIA
 
 from scipy import interpolate
-def calc_tau_cia(ISPACE,WAVE_GRID):
+def calc_tau_cia(K_CIA,ISPACE,WAVE_GRID,ID,ISO,VMR_layer):
+
+    # Need to pass NLAY from a atm profile
+    DELH = None
+    TOTAM = None
+    TEMPS = None
+    T_layer = None
+    AMAGAT = 2.68675E19 #mol cm-3
+
+
+    NLAY,NVMR = VMR_layer.shape
+
+    # mixing ratios of the relevant gases
+    qh2 = np.zeros(NLAY)
+    qhe = np.zeros(NLAY)
+    qn2 = np.zeros(NLAY)
+    qch4 = np.zeros(NLAY)
+    qco2 = np.zeros(NLAY)
+    IABSORB = np.ones(5,dtype='int32') * -1
+
+    for iVMR in range(NVMR):
+
+        if ID[iVMR] == 39: # hydrogen
+            qh2[:] = VMR_layer[:,iVMR]
+            IABSORB[0] = iVMR
+
+        if ID[iVMR] == 40: # helium
+            qhe[:] = VMR_layer[:,iVMR]
+            IABSORB[1] = iVMR
+
+        if ID[iVMR] == 22: # nitrogen
+            qn2[:] = VMR_layer[:,iVMR]
+            IABSORB[2] = iVMR
+
+        if ID[iVMR] == 6: # methane
+            qch4[:] = VMR_layer[:,iVMR]
+            IABSORB[3] = iVMR
+
+        if ID[iVMR] == 2: # co2
+            qco2[:] = VMR_layer[:,iVMR]
+            IABSORB[4] = iVMR
+
+        # calculating the opacity
+        XLEN = DELH * 1.0e2 # cm
+        TOTAM = TOTAM * 1.0e-4 # cm-2
+
+        amag1 = TOTAM / XLEN / AMAGAT # number density
+        tau = XLEN*amag1**2 # optical path, why fiddle around with XLEN
+
+        # define the calculatiion wavenumbers, WAVE_GRID IS WAVEC
+        if ISPACE == 0:
+            WAVEN = WAVE_GRID
+        elif ISPACE == 1:
+            WAVEN = 1.E4/WAVE_GRID
+            isort = np.argsort(WAVEN)
+            WAVEN = WAVEN[isort]
+
+
+        # if((WAVEN.min()<self.WAVEN.min()) or (WAVEN.max()>self.WAVEN.max())):
+        #     print('warning in CIA :: Calculation wavelengths expand a larger range than in .cia file')
+
+        # calculate the CIA opacity at the correct temperature and wavenumber
+        NWAVEC = len(WAVE_GRID)
+        tau_cia_layer = np.zeros([NWAVEC,NLAY])
+
+        for ilay in range(NLAY):
+
+            # interpolating to the correct temperature
+            temp1 = T_layer[ilay]
+            temp0,it = find_nearest(TEMPS,temp1)
+
+            # want to sandwich the T point
+            if TEMPS[it] >= temp1:
+                ithi = it
+                if it==0:
+                    temp1 = TEMPS[it]
+                    itl = 0
+                    ithi = 1
+                else:
+                    itl = it - 1
+
+            elif TEMPS[it]<temp1:
+                NT = len(TEMPS)
+                itl = it
+                if it == NT - 1:
+                    temp1 = TEMPS[it]
+                    ithi = NT - 1
+                    itl = NT - 2
+                else:
+                    ithi = it + 1
+
+
+            # find opacities for the chosen T
+            ktlo = K_CIA[:,itl,:]
+            kthi = K_CIA
+
+
+
+
+
+
+
+
 
 
 
