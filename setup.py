c9b4a531f02ab6ce562da4adfbb8338903b7ca64
Jingxuan Yang <jingxuan.yang@hertford.ox.ac.uk>
2022-08-30 17:54:28 +0100

    publish to pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup, find_packages
 
 VERSION = "0.0.1"
 DESCRIPTION = "Tools for modelling exoplanet spectra"
-LONG_DESCRIPTION = """Tools for analysing exoplanets emission spectra and \
-phase curves based on the Fortran Nemesis library"""
-
 setup(name="nemesispy",
       version=VERSION,
       description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
+      url='https://github.com/Jingxuan97/nemesispy2022',
       author="Jingxuan Yang",
       author_email="jingxuanyang15@gmail.com",
       packages=find_packages(),
