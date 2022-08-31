ba39883cd856db33315f848d810c290eceeddcd6
Jingxuan Yang <jingxuan.yang@hertford.ox.ac.uk>
2022-08-31 21:33:40 +0100

    wasp-43b gcm data

--- nemesispy/data/helper.py
+++ nemesispy/data/helper.py
@@ -27,6 +27,20 @@ del_g = np.array(
       dtype=np.float32)
 
 ### Low resolution k-table, HST/WFC3 + Spizter wavelengths
+ktable_path = os.path.join(__location__, "ktables")
+lowres_file_paths = [
+    'h2owasp43.kta',
+    'co2wasp43.kta',
+    'cowasp43.kta',
+    'ch4wasp43.kta']
+for ipath,path in enumerate(lowres_file_paths):
+    lowres_file_paths[ipath] = os.path.join(ktable_path,path)
+# k table information
+lowres_wavelengths = np.array(
+    [1.14250004, 1.17750001, 1.21249998, 1.24749994, 1.28250003,
+       1.3175    , 1.35249996, 1.38750005, 1.42250001, 1.45749998,
+       1.49249995, 1.52750003, 1.5625    , 1.59749997, 1.63250005,
+       3.5999999 , 4.5       ])
 lowres_pressure_grid = np.array(
     [3.0995553e-02, 8.6981423e-02, 2.4409227e-01, 6.8498516e-01,
        1.9222448e+00, 5.3943086e+00, 1.5137805e+01, 4.2480560e+01,
@@ -38,21 +52,6 @@ lowres_temperature_grid = np.array(
     [ 100.,  250.,  400.,  550.,  700.,  850., 1000., 1150., 1300.,
        1450., 1600., 1750., 1900., 2050., 2200., 2350., 2500., 2650.,
        2800., 2950.], dtype=np.float32)
-lowres_wavelengths = np.array(
-    [1.14250004, 1.17750001, 1.21249998, 1.24749994, 1.28250003,
-       1.3175    , 1.35249996, 1.38750005, 1.42250001, 1.45749998,
-       1.49249995, 1.52750003, 1.5625    , 1.59749997, 1.63250005,
-       3.5999999 , 4.5       ])
-
-ktable_path = os.path.join(__location__, "ktables")
-lowres_file_paths = [
-    'h2owasp43.kta',
-    'co2wasp43.kta',
-    'cowasp43.kta',
-    'ch4wasp43.kta']
-for ipath,path in enumerate(lowres_file_paths):
-    lowres_file_paths[ipath] = os.path.join(ktable_path,path)
-
 
 ### ARIEL k-tables
 ariel_file_paths = [
@@ -62,6 +61,31 @@ ariel_file_paths = [
     'CH4_Katy_ARIEL_test.kta']
 for ipath,path in enumerate(ariel_file_paths):
     ariel_file_paths[ipath] = os.path.join(ktable_path,path)
+# k table information
+ariel_wavelengths = np.array(
+    [0.55000001, 0.70499998, 0.95499998, 1.15638006, 1.27489996,
+       1.40558004, 1.54964995, 1.70849001, 1.88361001, 1.96959996,
+       2.00919008, 2.04957008, 2.09077001, 2.13279009, 2.17565989,
+       2.21938992, 2.26399994, 2.30950999, 2.35593009, 2.40328002,
+       2.45159006, 2.50086999, 2.55113006, 2.60241008, 2.65472007,
+       2.70808005, 2.76251006, 2.81803989, 2.87468004, 2.93246007,
+       2.9914    , 3.05152988, 3.11286998, 3.17544007, 3.23925996,
+       3.30436993, 3.37079   , 3.43853998, 3.50765991, 3.57816005,
+       3.65007997, 3.72344995, 4.03216982, 4.30545998, 4.59727001,
+       4.90887022, 5.24158001, 5.5968399 , 5.97618008, 6.38122988,
+       6.81373978, 7.2755599 ])
+ariel_pressure_grid = np.array(
+    [1.0132504e+00, 2.1829798e+00, 4.7030883e+00, 1.0132499e+01,
+       2.1829811e+01, 4.7030907e+01, 1.0132499e+02, 2.1829811e+02,
+       4.7030890e+02, 1.0132500e+03, 2.1829812e+03, 4.7030903e+03,
+       1.0132499e+04, 2.1829811e+04, 4.7030898e+04, 1.0132500e+05,
+       2.1829809e+05, 4.7030897e+05, 1.0132500e+06, 2.1829808e+06,
+       4.7030905e+06, 1.0132501e+07], dtype=np.float32)
+ariel_temperature_grid = np.array(
+    [ 100.,  200.,  300.,  400.,  500.,  600.,  700.,  800.,  900.,
+       1000., 1100., 1200., 1300., 1400., 1500., 1600., 1700., 1800.,
+       1900., 2000., 2200., 2400., 2600., 2800., 3000., 3200., 3400.],
+      dtype=np.float32)
 
 ### High resolution k-tables
 hires_file_paths = [
@@ -71,6 +95,21 @@ hires_file_paths = [
     'CH4_Katy_R1000.kta']
 for ipath,path in enumerate(hires_file_paths):
     hires_file_paths[ipath] = os.path.join(ktable_path,path)
+# k table information
+hires_pressure_grid = np.array(
+    [1.01324999e+00, 2.18298101e+00, 4.70308971e+00, 1.01324997e+01,
+       2.18298092e+01, 4.70308990e+01, 1.01325005e+02, 2.18298111e+02,
+       4.70308990e+02, 1.01325000e+03, 2.18298096e+03, 4.70308984e+03,
+       1.01325000e+04, 2.18298086e+04, 4.70308984e+04, 1.01325000e+05,
+       2.18298094e+05, 4.70308969e+05, 1.01325000e+06, 2.18298100e+06,
+       4.70309000e+06, 1.01325000e+07], dtype=np.float32)
+hires_temperature_grid = np.array(
+    [ 100.,  200.,  300.,  400.,  500.,  600.,  700.,  800.,  900.,
+       1000., 1100., 1200., 1300., 1400., 1500., 1600., 1700., 1800.,
+       1900., 2000., 2200., 2400., 2600., 2800., 3000., 3200., 3400.],
+      dtype=np.float32)
 
+
+### CIA tables
 cia_folder_path = os.path.join(__location__ , "cia")
 cia_file_path = os.path.join(cia_folder_path,'exocia_hitran12_200-3800K.tab')
--- nemesispy/plot/plot_compare_phase_curves.py
+++ nemesispy/plot/plot_compare_phase_curves.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import scipy.interpolate as interpolate
 import matplotlib.pyplot as plt
+from nemesispy.data.helper import lowres_file_paths, cia_file_path, \
+    lowres_wavelengths
 from nemesispy.radtran.forward_model import ForwardModel
 # Read GCM data
 from nemesispy.data.gcm.process_gcm import (nlon,nlat,xlon,xlat,npv,pv,\
@@ -10,17 +13,13 @@ from nemesispy.data.gcm.process_gcm import (nlon,nlat,xlon,xlat,npv,pv,\
     tmap_mod,h2omap_mod,comap_mod,co2map_mod,ch4map_mod,\
     hemap_mod,h2map_mod,vmrmap_mod,phase_grid,\
     kevin_phase_by_wave,kevin_wave_by_phase,\
-    pat_phase_by_wave,pat_wave_by_phase,\
-    vmrmap_mod_new,tmap_hot)
-import scipy.interpolate as interpolate
+    pat_phase_by_wave,pat_wave_by_phase)
+
 
 ### Reference Opacity Data
-lowres_files = [
-'/Users/jingxuanyang/Desktop/Workspace/nemesispy2022/nemesispy/data/ktables/h2o',
-'/Users/jingxuanyang/Desktop/Workspace/nemesispy2022/nemesispy/data/ktables/co2',
-'/Users/jingxuanyang/Desktop/Workspace/nemesispy2022/nemesispy/data/ktables/co',
-'/Users/jingxuanyang/Desktop/Workspace/nemesispy2022/nemesispy/data/ktables/ch4']
-cia_file_path='/Users/jingxuanyang/Desktop/Workspace/nemesispy2022/nemesispy/data/cia/exocia_hitran12_200-3800K.tab'
+lowres_files = lowres_file_paths
+cia_file_path = cia_file_path
+wave_grid = lowres_wavelengths
 
 ### Reference Spectral Input
 wasp43_spec = np.array([3.341320e+25, 3.215455e+25, 3.101460e+25, 2.987110e+25,
@@ -28,11 +27,8 @@ wasp43_spec = np.array([3.341320e+25, 3.215455e+25, 3.101460e+25, 2.987110e+25,
        2.505735e+25, 2.452230e+25, 2.391140e+25, 2.345905e+25,
        2.283720e+25, 2.203690e+25, 2.136015e+25, 1.234010e+24,
        4.422200e+23])
-wave_grid = np.array([1.1425, 1.1775, 1.2125, 1.2475, 1.2825, 1.3175, 1.3525, 1.3875,
-       1.4225, 1.4575, 1.4925, 1.5275, 1.5625, 1.5975, 1.6325, 3.6   ,
-       4.5   ])
-phase_grid = np.array([ 22.5,  45. ,  67.5,  90. , 112.5, 135. , 157.5, 180. , 202.5,
-       225. , 247.5, 270. , 292.5, 315. , 337.5])
+phase_grid = np.array([ 22.5,  45. ,  67.5,  90. , 112.5, 135. , 157.5,
+    180. , 202.5, 225. , 247.5, 270. , 292.5, 315. , 337.5])
 nwave = len(wave_grid)
 nphase = len(phase_grid)
 
@@ -47,8 +43,8 @@ nmu = 5
 
 ### Set up forward model
 FM = ForwardModel()
-FM.set_planet_model(M_plt=M_plt,R_plt=R_plt,gas_id_list=gas_id,iso_id_list=iso_id,
-    NLAYER=NLAYER)
+FM.set_planet_model(M_plt=M_plt,R_plt=R_plt,
+    gas_id_list=gas_id,iso_id_list=iso_id,NLAYER=NLAYER)
 FM.set_opacity_data(kta_file_paths=lowres_files, cia_file_path=cia_file_path)
 
 ### Set up TP profiles and spectral data arrays
