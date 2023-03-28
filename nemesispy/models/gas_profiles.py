75be1eaac45a0136c3aedd730ee7465ff8ca542c
Jingxuan Yang <jingxuan.yang@hertford.ox.ac.uk>
2023-03-28 11:47:40 +0100

    .

--- nemesispy/models/gas_profiles.py
+++ nemesispy/models/gas_profiles.py
@@ -2,6 +2,28 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+def gen_vmrmap_1d(h2o,co2,co,ch4,npress,h2_frac = 0.84):
+    """
+    Generate a 1D uniform gas abundance map.
+    The abundance map is defined on a pressure grid.
+
+    Parameters
+    ---------
+
+    Returns
+    -------
+    vmr_grid
+    """
+    he_frac = 1 - h2_frac
+    vmr_grid = np.ones((npress,6))
+    vmr_grid[:,0] *= 10**h2o
+    vmr_grid[:,1] *= 10**co2
+    vmr_grid[:,2] *= 10**co
+    vmr_grid[:,3] *= 10**ch4
+    vmr_grid[:,4] *= he_frac * (1-10**h2o-10**co2-10**co-10**ch4)
+    vmr_grid[:,5] *= h2_frac * (1-10**h2o-10**co2-10**co-10**ch4)
+    return vmr_grid
+
 def gen_vmrmap1(h2o,co2,co,ch4,nlon,nlat,npress,
         h2_frac = 0.84):
     """
@@ -13,7 +35,7 @@ def gen_vmrmap1(h2o,co2,co,ch4,nlon,nlat,npress,
 
     Returns
     -------
-        vmr_grid
+    vmr_grid
     """
     he_frac = 1 - h2_frac
     vmr_grid = np.ones((nlon,nlat,npress,6))
